b3bc1c5a0a731e618c901aea3938c934abb5c65a
cedricmenet <ced.menet@gmail.com>
2015-11-03 11:08:28 +0100

    Simple 3D mouse program

---
+++ Programme Principal/ConsoleApplicationImerir/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationImerir
+{
+   
+
+    class Program
+    {
+        static TDx.TDxInput.Device device;
+
+        static void Main(string[] args)
+        {
+            device = new TDx.TDxInput.Device();
+            device.Connect();
+
+            Console.WriteLine("Hello world");
+
+
+            while (true)
+            {
+                var translation = device.Sensor.Translation;
+                var rotation = device.Sensor.Rotation;
+
+                Console.WriteLine("translation X : " + translation.X);
+                Console.WriteLine("Rotation X : " + rotation.X);
+
+                System.Threading.Thread.Sleep(1000);
+            }
+
+        }
+    }
+}
